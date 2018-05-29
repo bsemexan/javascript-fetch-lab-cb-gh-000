bfce947a12b4d3b8b191db8a9152eb872284932a
Berry  Semexan <bsemexan@gmail.com>
2018-05-29 03:01:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ function showIssues(json) {
 }
 
 function createIssue() {
+  const username = ''
   const repo = 'api.github.com/repos/${''}/javascript-fetch-lab/issues'
   fetch(`${repo}`, {
     method: 'post',
