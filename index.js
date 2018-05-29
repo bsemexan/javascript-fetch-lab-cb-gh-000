48579e87047342a7fec50f4f7c2c8db3e722a179
Berry  Semexan <bsemexan@gmail.com>
2018-05-29 02:51:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function showResults(json) {
 function forkRepo() {
   const repo = 'api.github.com/repos/learn-co-curriculum/javascript-fetch-lab'
   //use fetch to fork it!
-  fetch(repo, {
+  fetch(`${repo}`, {
     headers: {
       Authorization: `token c99ad4b28b0936bd2beadbfaddcf249457714200`
     }
