098cbe25565c3a7f6ee460c615ff1ce6ebeeb308
Berry  Semexan <bsemexan@gmail.com>
2018-05-29 02:57:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ function showIssues(json) {
 }
 
 function createIssue() {
+  const repo = 'api.github.com/repos/learn-co-curriculum/javascript-fetch-lab/issues'
+  //use fetch to fork it!
+  fetch(`${repo}`, {
+    method: 'post',
+    headers: {
+      Authorization: `token c99ad4b28b0936bd2beadbfaddcf249457714200`
+    }
+  }).then(res => res.json()).then(json => console.log(json));
 }
 
 function showResults(json) {
